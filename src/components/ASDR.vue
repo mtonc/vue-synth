5ac4b11e8685282b01552d92ea81c88e79a88f6f
Mark <mark@cubicleninjas.com>
2017-04-26 13:44:17 -0500

    Saved ASDR before stopping

---
+++ src/components/ASDR.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="asdr">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'asdr',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
